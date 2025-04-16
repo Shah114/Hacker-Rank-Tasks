0be1d9d5de96a070429977abd55cf6c2487b67b4
Ibrahim <154831703+Shah114@users.noreply.github.com>
2025-04-16 16:17:02 +0400

    Add files via upload

---
+++ Solve_1/task_4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a;
+
+    cin >> a;
+
+
+    if (a % 2 == 0) {
+        cout << "The number is even." << endl;
+    } 
+    else {
+        cout << "The number is not even." << endl;
+    }
+    
+    return 0;
+}
