70ebc16ad73fa8a6abf58f3dbbcc7040bae4bc33
Ibrahim <154831703+Shah114@users.noreply.github.com>
2025-04-16 16:16:46 +0400

    Add files via upload

---
+++ Solve_1/task_3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a;
+    int b;
+
+    cin >> a >> b;
+
+    if (a > b) {
+        cout << a << " " << b << endl;
+    }
+    else if (b > a) {
+        cout << b << " " << a << endl;
+    }
+    else {
+        cout << "These 2 numbers are equal." << endl;
+    }
+    
+    return 0;
+}
