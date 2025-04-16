217c1099f5021ad55d3c0a219836141c194b1ec5
Ibrahim <154831703+Shah114@users.noreply.github.com>
2025-04-16 16:16:24 +0400

    Add files via upload

---
+++ Solve_1/task_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    
+    int a;
+    int b;
+    
+    cin >> a >> b;
+    
+    int c = b;
+    int d = a;
+    
+    cout << c << " " << d << endl;
+    
+    return 0;
+}
