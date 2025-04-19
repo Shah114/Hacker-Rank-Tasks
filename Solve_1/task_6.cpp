62303510dcfaf33c228eb3f7ef9ab0fbde38f057
Ibrahim <154831703+Shah114@users.noreply.github.com>
2025-04-19 14:01:25 +0400

    Add files via upload

---
+++ Solve_1/task_6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num;
+
+    cin >> num;
+
+    int a = num / 10;
+    int b = num % 10;
+
+    int sum = a + b;
+    int product = a * b;
+
+    cout << "Sum of the digits: " << sum << endl;
+    cout << "Product of the digits: " << product << endl;
+    
+    return 0;
+}
