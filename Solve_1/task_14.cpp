229e5e65eb63d1b43735cc72a0e61fceb3629793
Ibrahim <154831703+Shah114@users.noreply.github.com>
2025-05-03 22:55:28 +0400

    Add files via upload

---
+++ Solve_1/task_14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    int num;
+
+    cin >> num;
+
+    int a = num % 10;
+    int b = (num / 10) % 10;
+    int c = num / 100;
+
+    cout << b << c << a << endl;
+    
+    return 0;
+}
