e42dadaa52c0ef856f4418ff4a0209aa7dc4ecd5
Ibrahim <154831703+Shah114@users.noreply.github.com>
2025-06-11 10:13:47 +0400

    Add files via upload

---
+++ Solve_1/task_31.cpp
@@ -0,0 +1,27 @@
+// Minimum of Three Numbers
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    float a;
+    float b;
+    float c;
+    
+    cin >> a >> b >> c;
+    
+    if (a < b && a < c) {
+        cout << a << endl;
+    }
+    else if (b < a && b < c) {
+        cout << b << endl;
+    }
+    else if (c < a && c < b) {
+        cout << c << endl;
+    }
+    
+    return 0;
+}
