f750d110d5cf30e0385da4140c5be27aa2956710
Ibrahim <154831703+Shah114@users.noreply.github.com>
2025-06-11 10:19:49 +0400

    Add files via upload

---
+++ Solve_1/task_33.cpp
@@ -0,0 +1,23 @@
+// Number Comparison
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    float A;
+    float B;
+
+    cin >> A >> B;
+
+    if (A > B) {
+        cout << A << endl;
+    }
+    else {
+        cout << A << " " << B << endl;
+    }
+    
+    return 0;
+}
