b5836455a6120d1ef5250bad8d05338b0268bfbf
Ibrahim <154831703+Shah114@users.noreply.github.com>
2025-06-13 11:10:33 +0400

    Add files via upload

---
+++ Solve_1/task_46.cpp
@@ -0,0 +1,24 @@
+// Multiple Pre-Increments
+
+/*
+
+int main() {
+    int a = 1;
+    cout << ++a + a-- << endl;
+    cout << a << endl;
+}
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    cout << 4 << endl;
+    cout << 1 << endl;
+    
+    return 0;
+}
