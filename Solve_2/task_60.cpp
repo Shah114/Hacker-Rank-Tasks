f0b10d4a7839cde345edbc85267f0875610b2211
Ibrahim <154831703+Shah114@users.noreply.github.com>
2026-02-05 16:48:52 +0400

    Create task_60.cpp

---
+++ Solve_2/task_60.cpp
@@ -0,0 +1,24 @@
+// Divisors of a Natural Number
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    int N;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        if (N % i == 0) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;  
+    
+    return 0;
+}
