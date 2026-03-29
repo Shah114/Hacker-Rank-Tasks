52c1519ffc6ce8eb0c89b74f306ea5fc8c5e87c8
Ibrahim <154831703+Shah114@users.noreply.github.com>
2026-03-29 19:33:04 +0400

    Create task_97.cpp

---
+++ Solve_4/task_97.cpp
@@ -0,0 +1,15 @@
+// Pointer Type Mismatch
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    // YOUR_COMMENT: Write your explanation - The type of the value a pointer points to must be the same. For example if the variable a is an integer, so our pointer must also be of type integer. So the variable c is of type char, but the pointer is type of double. Therefore, even without running the code, the compiler can already detect this error.
+    
+
+    return 0;
+}
