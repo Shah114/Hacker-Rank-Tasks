dfcaa67cac0d39e38c991192e5fe7412123ec929
Ibrahim <154831703+Shah114@users.noreply.github.com>
2026-03-30 18:22:02 +0400

    Create tasl_100.cpp

---
+++ Solve_4/tasl_100.cpp
@@ -0,0 +1,33 @@
+// Array Sum using Pointers
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    int length;
+    cin >> length;
+    
+    int arr[length];
+    int *ptr = arr;
+    
+    for (int i = 0; i < length; ++i) {
+        cin >> *(ptr + i);
+    }
+    
+    int sum = 0;
+    
+    for (int i = 0; i < length; ++i) {
+        sum = sum + *(ptr + i);
+    }
+    
+    cout << "Sum of array elements: " << sum << endl;
+    
+    
+    return 0;
+}
